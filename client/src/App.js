e7524c9d81210d29a5b1135b533c5ea9afbadd4d
Alina Kovaleva <calipco.xp@gmail.com>
2023-06-08 23:49:48 +0200

    finalizing

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p></p>
+          <h1>Fib Calculator version KUBERNETES!</h1>
           <a
             className="App-link"
             href="https://reactjs.org"
